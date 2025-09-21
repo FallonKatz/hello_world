500ac63917cccf5282adc2bc43c4af0bfeff73d3
Fallon <fallon.katz@gmail.com>
2025-09-21 18:28:37 -0400

    Lab Assignment 4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
+use std::io;
+use rand;
+//use rand::prelude::*;
+use std::env;
+use std::fs;
+
+#[derive(Debug)]
+struct Shuttle {
+    name: String,
+    crew_size: u8,
+    propellant: f64
+}
+
+impl Shuttle {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn add_fuel(&mut self, gallons: f64) {
+        self.propellant += gallons;
+    }
+
+    fn new(name: &str) -> Shuttle {
+        Shuttle{
+            name: String::from(name),
+            crew_size: 7,
+            propellant: 0.0
+        }
+    }
+}
+
+struct Color(u8, u8, u8);   //RGB values
+struct Point(u8, u8, u8);    //XYZ values
+
+fn get_y(p: Point) -> u8 {
+    p.1
+}
+
 fn main() {
-    // Task 4
+    //Lab 2
+    /*// Task 4
     println!("Hello, world!");
 
     // Ch 2. Primitive Data Types
@@ -68,5 +107,94 @@ fn say_hello() {
 }
 
 fn celsius_to_far(temp: f64) -> f64 {
-    temp * 1.8 + 32.0 // Implicit return in Rust (no need for `return` keyword)
+    temp * 1.8 + 32.0 // Implicit return in Rust (no need for `return` keyword)*/
+
+    //Lab 4
+    //Ch 8. Modules
+    let mut buffer = String::new();
+    println!("Enter a message: ");
+    io::stdin().read_line(&mut buffer);
+    println!("buffer is {}", buffer);
+
+    let number: i32 = buffer.trim().parse().unwrap(); //Also parse::<i32> would work
+    println!("number + 1 is {}", number + 1);
+
+    /*In video, rand = "0.8.0" was added to toml
+    rand = "=0.9.2" was added to this toml
+    */
+   let number = rand::random::<f64>();
+   println!("number is {}", number);
+    
+    //let number = thread_rng().gen_range(1..11); //May be listed in the new version
+    //println!("number is {}", number);
+
+    //Ch 9. Input and Output
+    if env::args().len() <= 2 {
+        println!("Program requires at least 2 arguements"); //Prevents errors for line 108
+        return;
+    }
+    for (index, arguement) in env::args().enumerate() {
+        println!("arguement {} is {}", index, arguement);
+    }
+
+    let arg2 = env::args().nth(2).unwrap();
+    println!("arg2 is {}", arg2);
+
+
+    let contents = fs::read_to_string("planets.txt").unwrap();
+    println!("contents is {}", contents);
+
+    for line in contents.lines() {
+        println!("line is {}", line);
+    }
+
+    //let contents = fs::read("planets.txt").unwrap();    //Reads as vector
+    //println!("contents is {:?}", contents);   //THIS IS SO MUCH IT WORKS I SWEAR
+
+    let mut speech = String::new();
+    speech.push_str("We choose to go to the Moon in this decade\n");
+    speech.push_str("and do the other thins,\n");
+    speech.push_str("not because they are easy,\n");
+    speech.push_str("but because they are hard.");
+
+    //fs::write("speech.txt", speech);    //Will replace contents of existing files
+
+
+    //Ch. 10 Structs
+    let mut vehicle = Shuttle {
+        name: String::from("Endeavour"),
+        crew_size: 7,
+        propellant: 0.0
+    };
+    
+    let vehicle2 = Shuttle {
+        name: String::from("Discovery"),
+        ..vehicle
+    };
+    
+    let vehicle3 = Shuttle::new("Jerry");
+
+    //println!("name is {}", vehicle.name);
+
+    let vehicle_name = vehicle.get_name();  //Utilizes impl Shuttle
+    println!("vehicle_name is {}", vehicle_name);
+
+    vehicle.name = String::from("Atlantis");    //Can change specific aspects like this in Structs
+    vehicle.crew_size = 6;
+    println!("vehicle is {:?}", vehicle);
+    println!("vehicle2 is {:?}", vehicle2);
+
+    println!("propellant is {}", vehicle.propellant);
+    vehicle.add_fuel(10000.0);
+    println!("propellant is {}", vehicle.propellant);
+
+    println!("name is {}", vehicle3.name);
+
+
+    let red = Color(255, 0, 0);
+    println!("First value is {}", red.0);
+
+    let coord = Point(4, 5, 6); //Must be included, next line will be read for red instead
+    let y = get_y(coord);
+    println!("y is {}", y);
 }
